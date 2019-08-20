c9399e5cd128b50338d1c390ab739b7a0b3bf9f7
Ian Schwartz <ian.schwartz23@gmail.com>
2019-08-20 15:03:53 -0700

    closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,6 +3,15 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
+function football() {
+  const team = 'Broncos';
+  function showTeam() {
+    console.log(team);
+  }
+  return showTeam;
+}
+let myFunc = football();
+myFunc();
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
